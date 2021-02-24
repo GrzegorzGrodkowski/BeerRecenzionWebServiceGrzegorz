6aabcad9b8474b0b2d9dcc92d8bc03d03a34da9e
KooJaack <jacek.kanas@gmail.com>
2021-02-24 15:24:15 +0100

    repaired load app in gunicorn 3

--- instance/config.py
+++ instance/config.py
@@ -3,7 +3,4 @@ SQLALCHEMY_ECHO = True
 SECRET_KEY = 'Sm9obiBTY2hyb20ga2lja3MgYXNz'
 STRIPE_API_KEY = 'SmFjb2IgS2FwbGFuLU1vc3MgaXMgYSBoZXJv'
 UPLOADED_PHOTOS_DEST = '/uploads'
-MONGODB_USER = 'dbAdmin'
-MONGODB_PASSWORD = 'dbAdmin'
-MONGODB_HOST = 'cluster0.vlnxa.mongodb.net'
-MONGODB_DB = 'myFirstDatabase'
+MONGODB_HOST = "mongodb+srv://dbAdmin:dbAdmin@cluster0.vlnxa.mongodb.net/BeerRecenzionServiceDatabase?retryWrites=true&w=majority"
